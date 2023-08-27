92d75493129ad61b913f5b39ac4f3af986caa753
hebrohim <raheemibrohim@mail.com>
2023-08-27 01:18:13 +0100

    add key to mapped todos

--- src/components/DisplayTodos.tsx
+++ src/components/DisplayTodos.tsx
@@ -3,21 +3,12 @@ import { useContext } from "react";
 import { todoContext } from "../App";
 const DisplayTodos = () => {
   const todos = useContext(todoContext);
-  let days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
-let months = ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"]
-
-  let fullDate = new Date()
-  let date = fullDate.getDate()
-   let day = days[fullDate.getDay()]
-let month =months[ fullDate.getMonth()]
-
-let year = fullDate.getFullYear()
-
+ 
   return (
     <div className="bg-white mx-5 shadow-lg shadow-black rounded-lg -translate-y-10">
       {todos.map((todo, index) => {
         return (
-          <section>
+          <section key={index}>
            
             <div className=" p-5 flex justify-between ">
               <input type="checkbox" />
